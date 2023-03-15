f4c062f1b471730e11989933b76e98a579e637f8
evelyn <evelynkim.kr@gmail.com>
2023-03-15 21:54:09 +0900

    Feat - router: setting

--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFount from './pages/NotFount';
+import Home from './pages/Home';
+import AllWorks from './pages/AllWorks';
+import WorkDetail from './pages/WorkDetail';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFount />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/works', element: <AllWorks /> },
+      { 
+        path: '/works/:id', 
+        element: <WorkDetail />, 
+      },
+      { 
+        path: '/about', 
+        element: <About />
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
