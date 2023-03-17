f55b2c902292f7a133bef701ba7eda765b3302f6
evelyn <evelynkim.kr@gmail.com>
2023-03-17 18:13:33 +0900

    Add - Navbar.jsx: modify className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,35 @@ import React from "react";
 import styles from "../css/Navbar.module.css";
 import { Link } from "react-router-dom";
 // import { BiGift } from "react-icons/bi";
-import { IoGiftSharp } from "react-icons/io5";
+import { IoGiftSharp, IoMenu } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <header>
-      <button>
-        <IoGiftSharp />
+    <header id={styles.navbar}>
+      <button className={styles.toggle}>
+        <IoMenu className={styles.icon} />
       </button>
-      <Link to="/">Gift of You</Link>
-      <nav>
-        <Link to="/works">Works</Link>
-        <Link to="/about">About</Link>
-        <a href="#">Resume</a>
+      <nav className={styles.menu}>
+        <Link
+          to="/works"
+          className={`${styles.item} ${styles.active}`}
+          data-link="#works"
+        >
+          Works
+        </Link>
+        <Link to="/about" className={styles.item} data-link="#About">
+          About
+        </Link>
+        <a href="#" className={styles.item}>
+          Resume
+        </a>
+        <a href="#" className={styles.item} data-link="#Contact">
+          Contact
+        </a>
       </nav>
-      <a href="#">Contact</a>
+      <Link to="/" className={styles.logo} data-link="#home">
+        <IoGiftSharp className={styles.icon} />
+      </Link>
     </header>
   );
 }
