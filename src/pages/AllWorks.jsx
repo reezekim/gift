7a72205c12c183fc35451eeeb78bcabee2f31903
evelyn <evelynkim.kr@gmail.com>
2023-03-19 14:57:56 +0900

    Feat - AllWorks.jsx : add component and AllWorks.module.css

--- src/pages/AllWorks.jsx
+++ src/pages/AllWorks.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import styles from "../css/AllWorks.module.css";
 
 export default function AllWorks() {
-  return <div>AllWorks</div>;
+  return (
+    <section id={styles.works} className={styles.section}>
+      <div className={styles.intro}>
+        <h1 className={styles.title}>All Projects</h1>
+        {/* <p className={styles.desc}>총 4개의 프로젝트를 진행했습니다.</p> */}
+      </div>
+    </section>
+  );
 }
